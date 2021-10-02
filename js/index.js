c853ef95af9d39726fbb1563da33981fb8ef77b7
SuperCat2812 <Sashka281205@gmail.com>
2021-10-02 09:59:56 +0300

    main

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const obj = { id: 3, value: 8 };
+//const-изменять нельзя
+//obj- изменять можно
+
+let tims = {
+  'Иван': 100,
+  'Вася': 100,
+  'Коля':100,
+  'Гена': 90
+};
+let sum = [tims.Иван + tims.Вася+tims.Гена+tims.Коля];
+console.log(sum);
